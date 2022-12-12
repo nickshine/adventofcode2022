93b635e2a59984e03d5e4607a3cdf2a844d40e81
Nick Shine <shine.nick@gmail.com>
2022-12-12 08:26:19 -0600

    day 12

---
+++ day12/main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"math"
+	"strings"
+)
+
+//go:embed example.txt
+var exampleInput string
+
+//go:embed input.txt
+var input string
+
+type square struct {
+	x, y      int
+	elevation rune
+	score     int
+	visited   bool
+}
+
+func (s *square) String() string {
+	return fmt.Sprintf("Elevation: %c, score: %d\n", s.elevation, s.score)
+}
+
+type heightMap [][]*square
+
+func (h heightMap) String() string {
+	var sb strings.Builder
+	for _, row := range [][]*square(h) {
+		for _, col := range row {
+			sb.WriteRune(col.elevation)
+		}
+		sb.WriteString("\n")
+	}
+
+	return sb.String()
+}
+
+func readMap(in string) heightMap {
+	lines := strings.Split(strings.TrimSpace(in), "\n")
+	hmap := heightMap(make([][]*square, len(lines)))
+
+	for i, l := range lines {
+		hmap[i] = make([]*square, len(l))
+
+		for j, r := range l {
+			hmap[i][j] = &square{x: i, y: j, elevation: r}
+		}
+	}
+
+	return hmap
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func minSteps(paths []int) int {
+
+	min := math.MaxInt
+	for _, p := range paths {
+		if p < min {
+			min = p
+		}
+	}
+
+	return min
+}
+
+func visit(m heightMap, x, y int, prev *square, score int) {
+
+	if x < 0 || x >= len(m) || y < 0 || y >= len(m[x]) { // out of bounds
+		return
+	}
+
+	s := m[x][y]
+
+	if s.visited && s.score <= score { // shorter path already exists
+		return
+	}
+
+	if s.elevation-prev.elevation > 1 { // too steep to climb
+		return
+	}
+
+	s.visited = true
+	s.score = score
+
+	visit(m, x-1, y, s, score+1)
+	visit(m, x, y+1, s, score+1)
+	visit(m, x+1, y, s, score+1)
+	visit(m, x, y-1, s, score+1)
+}
+
+func part1(in string) int {
+	m := readMap(in)
+	var start, end *square
+
+	for _, row := range m {
+		for _, square := range row {
+			if square.elevation == 'S' {
+				start = square
+				start.elevation = 'a'
+			} else if square.elevation == 'E' {
+				end = square
+				end.elevation = 'z'
+			}
+		}
+	}
+
+	visit(m, start.x, start.y, start, 0)
+
+	return end.score
+}
+
+func part2(in string) int {
+	m := readMap(in)
+	var start, end *square
+	var startSquares []*square
+
+	for _, row := range m {
+		for _, square := range row {
+			if square.elevation == 'S' {
+				start = square
+				start.elevation = 'a'
+				startSquares = append(startSquares, start)
+			} else if square.elevation == 'E' {
+				end = square
+				end.elevation = 'z'
+			} else if square.elevation == 'a' {
+				startSquares = append(startSquares, square)
+			}
+		}
+	}
+
+	min := math.MaxInt
+	for _, s := range startSquares {
+		visit(m, s.x, s.y, s, 0)
+		if end.score < min {
+			min = end.score
+		}
+	}
+
+	return min
+}
+
+func main() {
+	fmt.Printf("Part 1 example: %d\n", part1(exampleInput))
+	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2 example: %d\n", part2(exampleInput))
+	fmt.Printf("Part 2: %d\n", part2(input))
+}
