510597a266b7f80b4af19134e9e4ac95c150de51
Nick Shine <shine.nick@gmail.com>
2022-12-12 10:14:54 -0600

    naming

--- day12/main.go
+++ day12/main.go
@@ -16,12 +16,12 @@ var input string
 type square struct {
 	x, y      int
 	elevation rune
-	score     int
+	steps     int
 	visited   bool
 }
 
 func (s *square) String() string {
-	return fmt.Sprintf("Elevation: %c, score: %d\n", s.elevation, s.score)
+	return fmt.Sprintf("Elevation: %c, steps: %d\n", s.elevation, s.steps)
 }
 
 type heightMap [][]*square
@@ -53,7 +53,7 @@ func readMap(in string) heightMap {
 	return hmap
 }
 
-func visit(m heightMap, x, y int, prev *square, score int) {
+func visit(m heightMap, x, y int, prev *square, steps int) {
 
 	if x < 0 || x >= len(m) || y < 0 || y >= len(m[x]) { // out of bounds
 		return
@@ -61,7 +61,7 @@ func visit(m heightMap, x, y int, prev *square, score int) {
 
 	s := m[x][y]
 
-	if s.visited && s.score <= score { // shorter path already exists
+	if s.visited && s.steps <= steps { // shorter path already exists
 		return
 	}
 
@@ -70,12 +70,12 @@ func visit(m heightMap, x, y int, prev *square, score int) {
 	}
 
 	s.visited = true
-	s.score = score
+	s.steps = steps
 
-	visit(m, x-1, y, s, score+1)
-	visit(m, x, y+1, s, score+1)
-	visit(m, x+1, y, s, score+1)
-	visit(m, x, y-1, s, score+1)
+	visit(m, x-1, y, s, steps+1)
+	visit(m, x, y+1, s, steps+1)
+	visit(m, x+1, y, s, steps+1)
+	visit(m, x, y-1, s, steps+1)
 }
 
 func part1(in string) int {
@@ -97,7 +97,7 @@ func part1(in string) int {
 
 	visit(m, start.x, start.y, start, 0)
 
-	return end.score
+	return end.steps
 }
 
 func part2(in string) int {
@@ -124,8 +124,8 @@ func part2(in string) int {
 	min := math.MaxInt
 	for _, s := range startSquares {
 		visit(m, s.x, s.y, s, 0)
-		if end.score < min {
-			min = end.score
+		if end.steps < min {
+			min = end.steps
 		}
 	}
 
