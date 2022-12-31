4a4d872f4fc0a85151d55caaf30cf138e299d216
Nick Shine <shine.nick@gmail.com>
2022-12-30 18:20:43 -0600

    add better snafu conversion

--- day25/main.go
+++ day25/main.go
@@ -37,11 +37,11 @@ func toSnafu(d int) string {
 	// find start place
 	var min, max int
 	place := 1
-	max = place * 5 / 2 // 2*place + place/2
+	max = 2*place + place/2
 
 	for abs(d) > max {
 		place *= 5
-		max = place * 5 / 2
+		max = 2*place + place/2
 	}
 
 	// determine digit for each place to 0
@@ -51,8 +51,8 @@ func toSnafu(d int) string {
 			sign = -1
 		}
 
-		min = place * 1 / 2 // place - place/2
-		max = place * 5 / 2 // 2*place + place/2
+		min = place - place/2   // place - place/2
+		max = 2*place + place/2 // 2*place + place/2
 
 		switch {
 		case abs(d) > max-place: // digit is 2 or -2
@@ -67,7 +67,38 @@ func toSnafu(d int) string {
 	}
 
 	return b.String()
+}
 
+func toSnafuBetter(d int) string {
+
+	var s string
+
+	const base = 5
+
+	for d > 0 {
+		c := byte('0')
+		switch d % base {
+		case 0:
+			c = decMap[0]
+		case 1:
+			c = decMap[1]
+		case 2:
+			c = decMap[2]
+		case 3:
+			c = decMap[-2]
+			d += base
+		case 4:
+			c = decMap[-1]
+			d += base
+		default:
+			panic("invalid")
+		}
+
+		s = string(c) + s
+		d /= base
+	}
+
+	return s
 }
 
 func part1(in string) string {
@@ -79,6 +110,7 @@ func part1(in string) string {
 	}
 
 	log.Printf("total: %d", total)
+	log.Printf("snafu traditional: %s", toSnafuBetter(total))
 	return toSnafu(total)
 }
 
@@ -86,67 +118,3 @@ func main() {
 	fmt.Printf("Part 1 example: %s\n", part1(exampleInput))
 	fmt.Printf("Part 1: %s\n", part1(input))
 }
-
-// rules:
-
-// 5^n == 2*(2*5^n-1 + 2*5^n-2 ... + 2*5^n-n) + 1
-
-// max for 5^n == 5^n + (5^n)/2
-//     max 5^4 == 5^4 + (5^4)/2
-//     max 625 == 625 + 312 == 937
-// snafu: 1222 == 1*5^4 + 2*5^3 + 2*5^2 + 2*5^1 + 2*5^0
-
-// min for 5^n == 5^n - (5^n)/2
-//     min 5^4 == 5^4 - 5^4/2
-//     min 625 == 625 - 312 == 313
-//                625 - 2*125 - 2*25 - 2*5 - 2*1
-
-// if 5^n - 5^n/2 > d
-// if 5^4 - 5^4/2 > d
-// if 625 - 312 > d
-// if 313 > d
-
-// if 5^n + 5^n/2 < d
-// if 5^4 + 5^4/2 < d
-// if 625 + 312 < d
-// if 937 < d
-// if 2*5^n + 5^n/2 < d
-// if 625+937 < d
-// if 1562 < d
-
-// 5^5 place:
-// if d within 1*5^n - 5^n/2 && 2*5^n+5^n/2
-// if d within 1*5^5 - 5^5/2 && 2*5^5+5^5/2
-// if d within  3125-1562 && 6250+1562
-// if d within  1563 && 7812
-
-// 5^4 place:
-// if d within 1*5^n - 5^n/2 && 2*5^n+5^n/2
-// if d within 1*5^4 - 5^4/2 && 2*5^4+5^4/2
-// if d within  625-312 && 1250+312
-// if d within  313 && 1562
-
-// 5^3 place:
-// if d within 5^3 - 5^3/2 && 2*5^3+5^3/2
-// if d within 125 - 62 && 250+62
-// if d within 63 && 312
-// 1 or 2?
-// if > 1*5^3+5^3/2, 2
-// if > 1*125+62
-// if > 187, 2
-// if > max-5^3 == 312-125 == 187, 2
-
-// 5^2 place:
-// if d within 5^2 - 5^2/2 && 2*5^2+5^2/2
-// if d within 25 - 12 && 50+12
-// if d within 13 && 62
-
-// 5^1 place:
-// if d within 5^1 - 5^1/2 && 2*5^1+5^1/2
-// if d within 5 - 2 && 10+2
-// if d within 3 && 12
-
-// 5^0 place:
-// if d within 5^0 - 5^0/2 && 2*5^0+5^0/2
-// if d within 1 - 0 && 2+0
-// if d within 1 && 2
